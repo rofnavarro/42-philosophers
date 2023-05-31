e21268b4570a009c837edd66f40db76504a38630
roferrero <rofnavarro@gmail.com>
2023-05-31 16:18:37 -0300

    print_status fix / no leaks

--- philo/utils/ft_print.c
+++ philo/utils/ft_print.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rferrero <rferrero@student.42sp.org.br     +#+  +:+       +#+        */
+/*   By: rferrero <rferrero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 22:52:57 by rferrero          #+#    #+#             */
-/*   Updated: 2023/05/28 23:04:04 by rferrero         ###   ########.fr       */
+/*   Updated: 2023/05/31 16:18:10 by rferrero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,13 @@ void	print_status(t_philos *philo, char *status)
 
 	pthread_mutex_lock(philo->data->death);
 	t = ft_time() - philo->data->time_start;
-	printf("%lld %d %s", ft_time() - philo->data->time_start, \
-				philo->id, status);
-	pthread_mutex_unlock(philo->data->death);
+	if (*philo->dead == FALSE)
+	{
+		printf("%lld %d %s", ft_time() - philo->data->time_start, \
+					philo->id, status);
+		pthread_mutex_unlock(philo->data->death);
+	}
+	else
+		printf("%lld %d died\n", ft_time() - philo->data->time_start, \
+					philo->id);
 }
